e8a0b6bf4338a8571ae52cdb688f53ebda98f2f2
Guilherme Garrucho <guilhermegarrucho@gmail.com>
2017-06-10 18:07:47 +0100

    Area and monster token done

--- src/gameInterface/graphicalInterface/DungeonPanel.java
+++ src/gameInterface/graphicalInterface/DungeonPanel.java
@@ -7,6 +7,8 @@ import gameLogic.states.AwaitBeginning;
 import gameLogic.states.AwaitOptionSelection;
 import gameLogic.states.AwaitTrading;
 import gameLogic.states.combatStates.AwaitDiceReroll;
+import gameLogic.states.combatStates.AwaitFeatDecision;
+import gameLogic.states.combatStates.AwaitSpellDecision;
 
 import javax.swing.*;
 import java.awt.*;
@@ -41,7 +43,8 @@ public class DungeonPanel extends JPanel implements Observer, Constants {
     public void update(Observable t, Object o){
         if( game.getState() instanceof AwaitBeginning ||
                 game.getState() instanceof AwaitTrading ||
-                game.getState() instanceof AwaitOptionSelection )
+                game.getState() instanceof AwaitOptionSelection ||
+                game.getState() instanceof AwaitDiceReroll)
             setVisible(false);
         else
             setVisible(true);
@@ -53,17 +56,25 @@ public class DungeonPanel extends JPanel implements Observer, Constants {
 
         g.drawImage(GraphicalPanel.getDungeonCard(), 0, 0, 360, 505, this);
 
-        if(game.getArea() <= 2)
-            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 1) * 40, 200, 30,30,this);
-        if(game.getArea() >= 3 && game.getArea() <= 4)
-            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 3) * 40, 260, 30,30,this);
-        if(game.getArea() >= 5 && game.getArea() <= 7)
-            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 5) * 40, 320, 30,30,this);
-        if(game.getArea() >= 8 && game.getArea() <= 10)
-            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 8) * 40, 380, 30,30,this);
-        if(game.getArea() >= 11)
-            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 11) * 40, 440, 30,30,this);
+        if (game.getArea() <= 2)
+            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 1) * 40, 200, 30, 30, this);
+        if (game.getArea() >= 3 && game.getArea() <= 4)
+            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 3) * 40, 260, 30, 30, this);
+        if (game.getArea() >= 5 && game.getArea() <= 7)
+            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 5) * 40, 320, 30, 30, this);
+        if (game.getArea() >= 8 && game.getArea() <= 10)
+            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 8) * 40, 380, 30, 30, this);
+        if (game.getArea() >= 11)
+            g.drawImage(GraphicalPanel.getToken(), 30 + (game.getArea() - 11) * 40, 440, 30, 30, this);
 
-       // g.drawImage(GraphicalPanel.getToken(), 305, 462 -(30 *2), 30,30,this);
+        if (game.getState() instanceof AwaitDiceReroll || game.getState() instanceof AwaitSpellDecision || game.getState() instanceof AwaitFeatDecision)
+            if(game.getMonsterHp() < 11)
+                g.drawImage(GraphicalPanel.getToken(), 305, 462 + (game.getMonsterHp() * 40), 30, 30, this);
+            if(game.getMonsterHp() > 10 && game.getMonsterHp() < 21)
+                g.drawImage(GraphicalPanel.getToken(), 263, 422 + (game.getMonsterHp() * 40) - 11, 30, 30, this);
+            if(game.getMonsterHp() > 20)
+                g.drawImage(GraphicalPanel.getToken(), 221, 422 + (game.getMonsterHp() * 40) - 21, 30, 30, this);
+        else
+            g.drawImage(GraphicalPanel.getToken(), 305, 462, 30, 30, this);
     }
 }
